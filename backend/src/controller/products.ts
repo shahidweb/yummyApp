03c85c863314aee521a79766a69ba1a88000b93c
shahidweb <shahidsaifi88@gmail.com>
2026-05-20 22:26:02 +0530

    BE002-UpdateAndDeleteProduct

--- backend/src/controller/products.ts
+++ backend/src/controller/products.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import { Product } from "../model/product.ts";
+import mongoose from "mongoose";
 
 export const getProducts = async (req: Request, res: Response) => {
     try {
@@ -24,6 +25,58 @@ export const createProduct = async (req: Request, res: Response) => {
             name, description, price, category, createdBy: userId
         })
         return res.status(201).json({ message: "Product created successfully!" })
+    } catch (error: any) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error
+        })
+    }
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id as any;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid product id" })
+        }
+        const { name, description, price, category } = req.body;
+
+        const updateData: any = {};
+        if (name !== undefined) updateData.name = name;
+        if (description !== undefined) updateData.description = description;
+        if (price !== undefined) updateData.price = price;
+        if (category !== undefined) updateData.category = category;
+        console.log(updateData)
+        const product = await Product.findByIdAndUpdate(id,
+            updateData,
+            { returnDocument: "after" }
+        );
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" })
+        }
+        return res.status(200).json({ message: "Product updated successfully!", product })
+
+    } catch (error: any) {
+        return res.status(500).json({
+            message: "Internal server error",
+            error
+        })
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id as any;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ message: "Invalid product id" })
+        }
+
+        const product = await Product.findByIdAndDelete(id);
+        if (!product) {
+            return res.status(404).json({ message: "Prodcut not found" })
+        }
+        return res.status(200).json({ message: "Product deleted successfully!" })
+
     } catch (error: any) {
         return res.status(500).json({
             message: "Internal server error",
--- backend/src/routes/product.ts
+++ backend/src/routes/product.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { createProduct } from "../controller/products.ts";
+import { createProduct, deleteProduct, getProducts, updateProduct } from "../controller/products.ts";
 import tokenValidate from "../middlerware/tokenValidate.ts";
 
 const router = express.Router();
 
+router.get('/', getProducts)
 router.post('/', tokenValidate, createProduct)
+router.put('/:id', tokenValidate, updateProduct)
+router.delete('/:id', tokenValidate, deleteProduct)
 
 export default router;
